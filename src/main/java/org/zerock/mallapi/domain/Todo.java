6f1b1646e0436d80c0da9f17ebddd515288757e5
DoHwan <jx7789@gmail.com>
2024-08-09 20:35:38 +0900

    feat.프로젝트 설정

---
+++ src/main/java/org/zerock/mallapi/domain/Todo.java
@@ -0,0 +1,29 @@
+package org.zerock.mallapi.domain;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tbl_todo")
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tno;
+
+    private String title;
+
+    private String writer;
+
+    private boolean complete;
+
+    private LocalDate dueDate;
+
+}
