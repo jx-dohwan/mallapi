592c51eb9bd7db57ed8b2fda66f2674ed33617d2
DoHwan <jx7789@gmail.com>
2024-08-10 12:11:30 +0900

    feat.서비스 계층의 구현

--- src/main/java/org/zerock/mallapi/service/TodoService.java
+++ src/main/java/org/zerock/mallapi/service/TodoService.java
@@ -5,4 +5,10 @@ import org.zerock.mallapi.dto.TodoDTO;
 public interface TodoService {
     Long register(TodoDTO todoDTO);
 
+    TodoDTO get(Long tno);
+
+    void modify(TodoDTO todoDTO);
+
+    void remove(Long tno);
+
 }
--- src/main/java/org/zerock/mallapi/service/TodoServiceImpl.java
+++ src/main/java/org/zerock/mallapi/service/TodoServiceImpl.java
@@ -1,20 +1,61 @@
 package org.zerock.mallapi.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.zerock.mallapi.domain.Todo;
 import org.zerock.mallapi.dto.TodoDTO;
+import org.zerock.mallapi.repository.TodoRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.Optional; // 올바른 Optional 임포트
 
 @Service
 @Transactional
 @Log4j2
+@RequiredArgsConstructor // 생성자 자동 주입
 public class TodoServiceImpl implements TodoService {
+    // 자동주입 대상은 final로
+    private final ModelMapper modelMapper;
+    private final TodoRepository todoRepository;
 
     @Override
     public Long register(TodoDTO todoDTO) {
-        log.info(".....");
+        log.info(".........");
+        Todo todo = modelMapper.map(todoDTO, Todo.class);
+        Todo savedTodo = todoRepository.save(todo);
+        return savedTodo.getTno();
+    }
 
-        return null;
+    @Override
+    public TodoDTO get(Long tno) {
+
+        Optional<Todo> result = todoRepository.findById(tno); // java.util.Optional 사용
+
+        Todo todo = result.orElseThrow();
+
+        TodoDTO dto = modelMapper.map(todo, TodoDTO.class);
+
+        return dto;
+    }
+
+    @Override
+    public void modify(TodoDTO todoDTO) {
+        Optional<Todo> result = todoRepository.findById(todoDTO.getTno());
+
+        Todo todo = result.orElseThrow();
+
+        todo.changeTitle(todoDTO.getTitle());
+        todo.changeDueDate(todoDTO.getDueDate());
+        todo.changeComplete(todoDTO.isComplete());
+
+        todoRepository.save(todo);
+    }
+
+    @Override
+    public void remove(Long tno) {
+        todoRepository.deleteById(tno);
     }
 }
